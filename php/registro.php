ea56e858378c9612acfa2320ef9a2e3a48a83297
Mathias <Mathias@DESKTOP-01MNNDE>
2016-08-27 20:05:39 -0300

    agregado registro

--- php/registro.php
+++ php/registro.php
@@ -8,17 +8,41 @@
     $password = filter_input(INPUT_POST, "password");
     //imagen
     
+    $errores = "Se detectaron los siguientes errores:<br>";
     $usuarioValido = true;
     
     if (!preg_match("/^[a-zA-Z ]*$/",$nombre)){
-        $nombreErrorLetras = "El nombre debe estar compuesto unicamente por letras"; 
+        $errores .= "El nombre debe estar compuesto unicamente por letras<br>"; 
         $usuarioValido = false;
     }
     if(strlen($nombre)>20){
-        $nombreErrorLargo = "El nombre debe tener un largo menor a 20 caracteres";
+        $errores .= "El nombre debe tener un largo menor a 20 caracteres<br>";
         $usuarioValido = false;
     }
+    if(empty($biografia)){
+        $errores .= "La biografia no puede ser vacia<br>";
+        $usuarioValido = false;
+    }
+    if(filter_var($email, FILTER_VALIDATE_EMAIL)==false){
+        $errores .= "El mail ingresado no es valido<br>";
+        $usuarioValido = false;
+    }
+    if(strlen($password)<6){
+        $errores .= "La contrasenia debe tener mas de 6 caracteres<br>";
+        $usuarioValido = false;
+    }
+    //validacion imagen (puede ser null)
     
+    if($usuarioValido){
+        //se registra en BD
+    }
+    else{
+        ?>
+        <h3>Sus datos son incorrectos. Intentelo nuevamente.</h3>
+        <a href="../index.php">Volver</a>
+<?php
+        
+    }
     
 ?>
 
